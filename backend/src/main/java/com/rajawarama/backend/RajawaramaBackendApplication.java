5f4aff73c57355e64ff486dc5104abdf2b300c91
Avishka Niroshana <avishkanperera4@gmail.com>
2026-01-02 02:32:43 +0530

    Initial Spring Boot bavkend setup

---
+++ backend/src/main/java/com/rajawarama/backend/RajawaramaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rajawarama.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajawaramaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajawaramaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rajawarama/backend/RajawaramaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajawarama.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajawaramaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

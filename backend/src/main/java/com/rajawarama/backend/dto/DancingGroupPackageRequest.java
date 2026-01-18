78293010d9853541fa85f9a1fc48395b1fadd8f6
Avishka Niroshana <avishkanperera4@gmail.com>
2026-01-19 00:30:25 +0530

    Changed some files in backend

--- backend/src/main/java/com/rajawarama/backend/dto/DancingGroupPackageRequest.java
+++ backend/src/main/java/com/rajawarama/backend/dto/DancingGroupPackageRequest.java
@@ -1,2 +1,20 @@
-package com.rajawarama.backend.dto;public class DancingGroupPackageRequest {
+package com.rajawarama.backend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class DancingGroupPackageRequest {
+
+
+    @NotBlank(message = "Package name is required!")
+    private String name;
+
+    @NotBlank(message = "Details are required!")
+    private String details;
+
+    @NotNull(message = "Price is required!")
+    private Double price;
+
 }
--- backend/src/main/java/com/rajawarama/backend/repository/DancingGroupPackageRepository.java
+++ backend/src/main/java/com/rajawarama/backend/repository/DancingGroupPackageRepository.java
@@ -1,2 +1,11 @@
-package com.rajawarama.backend.repository;public interface DancingPackageRepository {
+package com.rajawarama.backend.repository;
+
+import com.rajawarama.backend.entity.DancingGroupPackage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface DancingGroupPackageRepository extends JpaRepository<DancingGroupPackage, UUID> {
+    Optional<DancingGroupPackage> findByNameIgnoreCase(String name);
 }

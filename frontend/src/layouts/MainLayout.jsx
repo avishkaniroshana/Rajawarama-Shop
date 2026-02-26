51caa313f5a8b1c90ffd367a542e0941dd9b4361
Avishka Niroshana <avishkanperera4@gmail.com>
2026-02-26 18:26:45 +0530

    changed

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,52 +1,15 @@
-import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import React from "react";
 import Header from "../components/common/Header/Header.jsx";
 import Footer from "../components/common/Footer/Footer.jsx";
-import AdminSidebar from "../components/admin/AdminSidebar";
 
-function MainLayout({ children }) {
-  const location = useLocation();
-  const [collapsed, setCollapsed] = useState(false);
-
-  const isAdminRoute = location.pathname.startsWith("/admin");
-
+function MainLayout  ({ children }) {
   return (
     <>
-<Header />
-      <div className="flex">
-        {/* Admin Sidebar (only for admin pages) */}
-        {isAdminRoute && (
-          <AdminSidebar collapsed={collapsed} setCollapsed={setCollapsed} />
-        )}
-
-        {/* Main Content */}
-        <main
-          className={`min-h-screen transition-all duration-300 w-full
-          ${isAdminRoute ? (collapsed ? "ml-[90px]" : "ml-[260px]") : ""}`}
-        >
-          {children}
-        </main>
-      </div>
-
-<Footer/>    
-</>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
   );
-}
+};
 
 export default MainLayout;
-
-// import React from "react";
-// import Header from "../components/common/Header/Header.jsx";
-// import Footer from "../components/common/Footer/Footer.jsx";
-
-// function MainLayout  ({ children }) {
-//   return (
-//     <>
-//       <Header />
-//       <main className="min-h-screen">{children}</main>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default MainLayout;
